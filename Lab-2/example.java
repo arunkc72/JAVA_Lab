3daff67f8907b891deaa2407723d0b0f34b0d860
arunkc72 <53501414+arunkc72@users.noreply.github.com>
2020-02-10 21:53:05 +0545

    Create example.java

---
+++ Lab-2/example.java
@@ -0,0 +1,14 @@
+  
+class Example
+{
+      private static int var;
+      public Example(int num)
+      {
+             var=num;
+      }
+      public static void main(String args[])
+      {
+              Example obj = new Example(2);
+              System.out.println("value of var is:"+ var);
+      }
+}
